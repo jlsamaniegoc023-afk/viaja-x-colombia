0fc67ff8a3b557ced2f058771342ddab39d59ac9
Jose Luis Samaniego <jlsamaniegoc023@gmail.com>
2026-04-25 11:48:05 -0500

    Entrega final Angular ViajaXColombia

---
+++ src/app/pages/destinos/destinos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destinos',
+  imports: [],
+  templateUrl: './destinos.html',
+  styleUrl: './destinos.css',
+})
+export class Destinos {}

31b4d9fe9b8774004197a920a97822d0e127ab28
Jose Luis Samaniego <jlsamaniegoc023@gmail.com>
2026-04-25 18:50:32 -0500

    Migrado a HTML puro para Vercel

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `
+    <h2>Inicio</h2>
+    <p>Bienvenido a Viaja X Colombia</p>
+  `
+})
+export class HomeComponent {}
